0640000832f654aa1fe3b0a31dd6dd1cb0e45111
nazzykitty8 <163137135+nazzykitty8@users.noreply.github.com>
2024-03-12 09:39:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/736x/a1/9d/42/a19d423fa59296cc4f72f4cefa598f20.jpg"}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/1200x/3b/b8/0f/3bb80f5a3a5f90a087e3869081e6d6f2.jpg";
+}
+
+
+
+
